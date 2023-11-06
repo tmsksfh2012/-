d4da3417056aa71fee8b388db572fde979ac639c
tmsksfh2012 <tmsksfh2012@naver.com>
2023-11-06 17:10:33 +0900

    first commit

---
+++ 1이 될 때까지.py
@@ -0,0 +1,15 @@
+N,K = map(int, input().split(","))
+result = 0
+
+while True:
+    target = (N//K) * K
+    result += (N - target)
+    N = target
+    
+    if N < K:
+        break
+    result += 1
+    N //= K
+    
+result += (N - 1)
+print(result)
